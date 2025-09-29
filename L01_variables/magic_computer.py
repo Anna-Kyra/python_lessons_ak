02e2837448c241bdaeedac86ab505092d4a5a4b8
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-09-29 10:38:11 +0200

    magic computer

---
+++ L01_variables/magic_computer.py
@@ -0,0 +1,5 @@
+print("Think of any whole number, write it down below and I will guess it!")
+number = int(input(">: "))
+print(f"The number in your head is {number}!\n"
+      f"\tAmazing isn't it?\n"
+      f"\t\t This is why computers will take over the world.")
