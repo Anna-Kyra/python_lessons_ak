119a9007d91c2c2678a89277009d399649bcb464
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-10-04 20:33:19 +0200

    refactor: special characters
    kan \' en \" gebruiken in een string

--- L01_variables/special_characters.py
+++ L01_variables/special_characters.py
@@ -1,4 +1,4 @@
 print("I'm so good at Python \n"
-      "I can use '' in a string\n" +
-      'I can use "" in a string \n'
-      'I can even use both " as well as' + " '.")
+      "I can use '' in a string\n"
+      "I can use \"\" in a string \n"
+      "I can even use both \" as well as\'.")
