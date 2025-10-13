db9737c169c82682958ca85437aa8644fe80a2ce
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-10-13 11:39:32 +0200

    L03: practice functions (time for a compliment)

--- L03_functions_continued/practice_functions.py
+++ L03_functions_continued/practice_functions.py
@@ -1,4 +1,7 @@
 #PRINT TITLE
+import random
+
+
 def print_title(title: str, symbol: str, times: int):
     """
     Prints title surrounded by a certain symbol
@@ -11,4 +14,21 @@ def print_title(title: str, symbol: str, times: int):
 
 print_title("Hello world", "*", 10)
 print_title("Bye world", "=", 5)
+
+def print_compliment() -> str:
+    adjectives = ("artful", "nimble", "brilliant", "expressive", "colorful", "clever",
+                  "graceful", "innovative", "vibrant")
+    nouns = ("programmer", "designer", "coder", "artist", "creator", "developer",
+             "visionary", "architect", "maker", "painter")
+
+    adjective = random.choice(adjectives)
+    noun = random.choice(nouns)
+    return f"{adjective} {noun}"
+
+compliment = print_compliment()
+print(f"Wow, you are one {compliment}!")
+print(f"And did you know your neighbour is a {compliment}?")
+
+
+
 
