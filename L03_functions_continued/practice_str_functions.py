bb183e4a71f58691d3312f4e26b24e4c291db41e
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-10-19 14:57:56 +0200

    L03: Movie name
    heb replace gebruikt zodat of en the altijd met lage letters worden geschreven. Weet niet of dit de beste manier is

---
+++ L03_functions_continued/practice_str_functions.py
@@ -0,0 +1,5 @@
+#MOVIE NAME
+movie_name = input("Enter a movie name: ") #LORD OF THE RINGS : fellowship of the ring
+print(f"* WHATCH NOW * {movie_name.title().replace('Of', 'of').replace('The', 'the')}")
+
+#WORD GUESS
