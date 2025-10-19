660b407b86123de76079df9f34fa9460fc428434
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-10-19 15:37:44 +0200

    L03: Hidden word
    string functions: count, find, rfind

--- L03_functions_continued/practice_str_functions.py
+++ L03_functions_continued/practice_str_functions.py
@@ -3,13 +3,23 @@
 # print(f"* WHATCH NOW * {movie_name.title().replace('Of', 'of').replace('The', 'the')}")
 
 #WORD GUESS
-word_guess = input("Enter a word to guess: ")
-print(f"* This word is secret: "
-      f"{word_guess.lower()
-      .replace('a', '*')
-      .replace('e', '*')
-      .replace('i', '*')
-      .replace('o', '*')
-      .replace('u', '*')}\n"
-      f".\n.\n.\n.\n"
-      f"* psst.. don't tell anyone.. the actual word is '{word_guess}'!")
+# word_guess = input("Enter a word to guess: ")
+# print(f"* This word is secret: "
+#       f"{word_guess.lower()
+#       .replace('a', '*')
+#       .replace('e', '*')
+#       .replace('i', '*')
+#       .replace('o', '*')
+#       .replace('u', '*')}\n"
+#       f".\n.\n.\n.\n"
+#       f"* psst.. don't tell anyone.. the actual word is '{word_guess}'!")
+
+#HIDDEN WORD
+hidden_word = "occurrence"
+print("*" * len(hidden_word))
+
+character = input(f"Please enter a single character you want to find in my hidden word {hidden_word}: ")
+print(f"* NUMBER OF TIMES THE CHARACTER IS IN THIS WORD: {hidden_word.count(character.lower())}")
+print(f"* FIRST INDEX OF THE CHARACTER IN THIS WORD: {hidden_word.find(character.lower())}")
+print(f"* FIRST INDEX OF THE CHARACTER IN THIS WORD: {hidden_word.rfind(character.lower())}")
+
