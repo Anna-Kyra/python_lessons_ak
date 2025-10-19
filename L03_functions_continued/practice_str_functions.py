c9cc2072c60271b7af59a97d20ddd61726062129
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-10-19 15:23:38 +0200

    L03: word guess
    Had replace gebruikt, dacht dat je replace str function ook met een array kan doen om het zo dryer te maken, maar kan niet.
    
    Inplaats van replace kan ook de maketrans function worden gebruikt:
    ' vervangingen = str.maketrans({
        "a": "o",
        "e": "u",
        "i": "y"
    })
    '

--- L03_functions_continued/practice_str_functions.py
+++ L03_functions_continued/practice_str_functions.py
@@ -1,5 +1,15 @@
 #MOVIE NAME
-movie_name = input("Enter a movie name: ") #LORD OF THE RINGS : fellowship of the ring
-print(f"* WHATCH NOW * {movie_name.title().replace('Of', 'of').replace('The', 'the')}")
+# movie_name = input("Enter a movie name: ") #LORD OF THE RINGS : fellowship of the ring
+# print(f"* WHATCH NOW * {movie_name.title().replace('Of', 'of').replace('The', 'the')}")
 
 #WORD GUESS
+word_guess = input("Enter a word to guess: ")
+print(f"* This word is secret: "
+      f"{word_guess.lower()
+      .replace('a', '*')
+      .replace('e', '*')
+      .replace('i', '*')
+      .replace('o', '*')
+      .replace('u', '*')}\n"
+      f".\n.\n.\n.\n"
+      f"* psst.. don't tell anyone.. the actual word is '{word_guess}'!")
