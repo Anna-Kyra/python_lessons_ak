95d39292d9184ac13dc50d9670d54caf8a83fc6f
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-12-15 11:33:22 +0100

    L10: refactor type_word function

--- L10_project/type_more_words_engine_test.py
+++ L10_project/type_more_words_engine_test.py
@@ -120,68 +120,59 @@ def mouse_pressed_event(mouse_x: int, mouse_y: int, mouse_button: MouseButton):
 
     pass
 
-def type_word_cow(key: str):
+def type_word(animal: str, key: str):
     global current_index_cow, random_word_cow, random_word_list_cow, word_colored_cow
-
-    correct = False
-    word_colored_cow = random_word_cow[:current_index_cow]
-
-    # COW
-    if key.lower() == random_word_list_cow[current_index_cow]:
-        correct = True
-        current_index_cow += 1
-        word_colored_cow = random_word_cow[:current_index_cow]
-
-    if current_index_cow == len(random_word_list_cow):
-        print("Cow word complete!")
-        random_word_cow = random.choice(word_list_cow)
-        random_word_list_cow = list(random_word_cow)
-        current_index_cow = 0
-        word_colored_cow = ""
-
-    if not correct:
-        print("Wrong:", key)
-
-def type_word_chicken(key: str):
     global current_index_chicken, random_word_chicken, random_word_list_chicken, word_colored_chicken
-
-    correct = False
-    word_colored_chicken = random_word_chicken[:current_index_chicken]
-
-    # COW
-    if key.lower() == random_word_list_chicken[current_index_chicken]:
-        correct = True
-        current_index_chicken += 1
-        word_colored_chicken = random_word_chicken[:current_index_chicken]
-
-    if current_index_chicken == len(random_word_list_chicken):
-        print("Cow word complete!")
-        random_word_chicken = random.choice(word_list_chicken)
-        random_word_list_chicken = list(random_word_chicken)
-        current_index_chicken = 0
-        word_colored_chicken = ""
-
-    if not correct:
-        print("Wrong:", key)
-
-def type_word_horse(key: str):
     global current_index_horse, random_word_horse, random_word_list_horse, word_colored_horse
 
     correct = False
-    word_colored_horse = random_word_horse[:current_index_horse]
 
     # COW
-    if key.lower() == random_word_list_horse[current_index_horse]:
-        correct = True
-        current_index_horse += 1
+    if animal == "cow":
+        word_colored_cow = random_word_cow[:current_index_cow]
+
+        if key.lower() == random_word_list_cow[current_index_cow]:
+            correct = True
+            current_index_cow += 1
+            word_colored_cow = random_word_cow[:current_index_cow]
+
+        if current_index_cow == len(random_word_list_cow):
+            print("Cow word complete!")
+            random_word_cow = random.choice(word_list_cow)
+            random_word_list_cow = list(random_word_cow)
+            current_index_cow = 0
+            word_colored_cow = ""
+    # CHICKEN
+    elif animal == "chicken":
+        word_colored_chicken = random_word_chicken[:current_index_chicken]
+
+        if key.lower() == random_word_list_chicken[current_index_chicken]:
+            correct = True
+            current_index_chicken += 1
+            word_colored_chicken = random_word_chicken[:current_index_chicken]
+
+        if current_index_chicken == len(random_word_list_chicken):
+            print("Cow word complete!")
+            random_word_chicken = random.choice(word_list_chicken)
+            random_word_list_chicken = list(random_word_chicken)
+            current_index_chicken = 0
+            word_colored_chicken = ""
+    # HORSE
+    elif animal == "horse":
         word_colored_horse = random_word_horse[:current_index_horse]
 
-    if current_index_horse == len(random_word_list_horse):
-        print("Cow word complete!")
-        random_word_horse = random.choice(word_list_horse)
-        random_word_list_horse = list(random_word_horse)
-        current_index_horse = 0
-        word_colored_horse = ""
+        if key.lower() == random_word_list_horse[current_index_horse]:
+            correct = True
+            current_index_horse += 1
+            word_colored_horse = random_word_horse[:current_index_horse]
+
+        if current_index_horse == len(random_word_list_horse):
+            print("Cow word complete!")
+            random_word_horse = random.choice(word_list_horse)
+            random_word_list_horse = list(random_word_horse)
+            current_index_horse = 0
+            word_colored_horse = ""
+
 
     if not correct:
         print("Wrong:", key)
@@ -191,9 +182,9 @@ def key_up_event(key: str):
     This function is only executed once each time a key was released!
     Special keys have more than 1 character, for example ESCAPE, BACKSPACE, ENTER, ...
     """
-    type_word_cow(key)
-    type_word_chicken(key)
-    type_word_horse(key)
+    type_word("cow", key)
+    type_word("chicken", key)
+    type_word("horse", key)
     pass
 
 
